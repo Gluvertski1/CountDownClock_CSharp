e9ed59e217f0a7cc762eae624235444189226d45
Jared <73083139+Gluvertski1@users.noreply.github.com>
2021-01-15 13:42:52 -0500

    Add files via upload

---
+++ CountDownClock/Clock.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Tester
+{
+    class Clock
+    {
+        // creating the time variables.
+        private int second;
+
+        // the delegate the subscribers must implement
+        public delegate void SecondChangedHandler(object clock, TimeInfoEventArgs timeInformation);
+
+        public event SecondChangedHandler SecondChanged;
+
+        public void Run(int waitTime)
+        {
+            do
+            {
+                Thread.Sleep(waitTime*1000);
+
+                System.DateTime dt = System.DateTime.Now;
+
+                if (dt.Second != second)
+                {
+                    TimeInfoEventArgs timeInformation = new TimeInfoEventArgs(dt.Second);
+
+                    if (SecondChanged != null)
+                    {
+                        SecondChanged(this, timeInformation);
+                    }
+                }
+
+
+                //update the state
+                this.second = dt.Second;
+
+            }
+            while (second <= waitTime);
+        }
+    }
+}
---
+++ CountDownClock/DisplayClock.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tester
+{
+    class DisplayClock
+    {
+        // given a clock, subscribe to its SecondChangedHandler event
+        // HW 17-1 method
+        //public void Subscribe(Clock theClock)
+        //{
+        //    theClock.SecondChanged += TimeHasChanged;
+
+        //}
+
+        //public void TimeHasChanged(object theClock, TimeInfoEventArgs ti)
+        //{
+        //    //Console.WriteLine("Current Time: {0}", ti.second.ToString()); 
+        //}
+
+        // Anonymous method style
+        //----------------------------------
+        //public void Subscribe(Clock clock)
+        //{
+        //    clock.SecondChanged += delegate(object theClock, TimeInfoEventArgs ti)
+        //    {
+        //        Console.WriteLine("Current Time: {0}", ti.second.ToString());
+        //    };
+        //}
+        ////----------------------------------
+
+
+        // HW 17-4 Style Lambda Expression
+        //-----------------------------------
+        public void Subscribe(Clock clock)
+        {
+            clock.SecondChanged += 
+            (aclock, ti) => 
+            {
+             Console.WriteLine("Current Time: {0}", ti.second.ToString()); 
+            };
+        }
+        //----------------------------------
+    }
+}
